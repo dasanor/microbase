79268bfc2224c4219e4d67a8438c187707615558
Nicolás Cornaglia <ncornag@gmail.com>
2016-05-19 22:25:35 +0200

    Added code documentation tool.

---
+++ docs/doc-filelist.js
@@ -0,0 +1,1 @@
+var tree={"files":["index.js"],"dirs":{"modules":{"dirs":{"config":{"files":["index.js"]},"db":{"files":["index.js"]},"logger":{"files":["index.js"]},"services":{"files":["index.js"]},"workers":{"files":["index.js"]}}}}};
