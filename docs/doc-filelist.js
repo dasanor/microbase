0ef97be265c8139b1cbfb6845f160f8151c17618
Nicolás Cornaglia <ncornag@gmail.com>
2016-06-01 19:33:21 +0200

    Removed generated docs from git

--- docs/doc-filelist.js
+++
@@ -1,1 +0,0 @@
-var tree={"files":["index.js"],"dirs":{"modules":{"dirs":{"config":{"files":["index.js"]},"db":{"files":["index.js"]},"logger":{"files":["index.js"]},"services":{"files":["index.js"]},"workers":{"files":["index.js"]}}}}};
