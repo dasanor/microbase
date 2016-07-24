216cdca4dc40bbd0d3228604c255f52bc9db57ff
Nicolás Cornaglia <ncornag@gmail.com>
2016-07-24 14:18:31 +0200

    Remove PHP dependency in documentation

--- ecomm/generatedocs.sh
+++ ecomm/generatedocs.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-cp ../README.md docs/sources/pages/index.md
-cp services/micro-cart-service/README.md docs/sources/pages/micro-cart-service.md
-cp services/micro-catalog-service/README.md docs/sources/pages/micro-catalog-service.md
-cp services/micro-stock-service/README.md docs/sources/pages/micro-stock-service.md
-cp services/micro-oauth-service/README.md docs/sources/pages/micro-oauth-service.md
+cp ../README.md docs/sources/index.md
+cp services/micro-cart-service/README.md docs/sources/micro-cart-service.md
+cp services/micro-catalog-service/README.md docs/sources/micro-catalog-service.md
+cp services/micro-stock-service/README.md docs/sources/micro-stock-service.md
+cp services/micro-oauth-service/README.md docs/sources/micro-oauth-service.md
 
 npm run doc
