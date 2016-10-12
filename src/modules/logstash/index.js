4de83bbd025f1e6ac5ccf8b9996ee063120dc73a
Nicolás Cornaglia <ncornag@gmail.com>
2016-10-12 21:37:12 +0200

    Fixed docker-compose logstash. Refatored logstash service.

--- src/modules/logstash/index.js
+++ src/modules/logstash/index.js
@@ -1,20 +1,14 @@
 const winston = require('winston');
 
 module.exports = function (base) {
-
-    if (!base.config.get('logstash')) {
-        base.logger.info('[logstash] database properties not configured');
-        return
-    }
-
-    require('winston-logstash');
-
-    var logstashConfig = base.config.get('logstash');
-
-    base.logger.debug('[logstash] Current logstash configuration: ' + JSON.stringify(logstashConfig));
-
-
-    base.logger.add(winston.transports.Logstash, {
-        logstashConfig
-    });
+  if (!base.config.get('logstash')) {
+    base.logger.info('[logstash] database properties not configured');
+    return;
+  }
+  const logstashConfig = base.config.get('logstash');
+  //console.log(logstashConfig);
+  base.logger.info(`[logstash] connecting to [${logstashConfig.host}:${logstashConfig.port}]`);
+  
+  require('winston-logstash');
+  base.logger.add(winston.transports.Logstash, logstashConfig);
 };
