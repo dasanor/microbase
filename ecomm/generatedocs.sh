c9eac05f6dcef5c494ccde96df73b97bf3df27d4
Nicolás Cornaglia <ncornag@gmail.com>
2016-12-06 22:01:59 +0200

    Added Personalization and Customer readme to the docs.

--- ecomm/generatedocs.sh
+++ ecomm/generatedocs.sh
@@ -11,18 +11,22 @@ DEST=$1
 cp ../README.md docs/sources/index.md
 
 cp $DEST/micro-catalog-service/README.md docs/sources/micro-catalog-service.md
+cp $DEST/micro-customer-service/README.md docs/sources/micro-customer-service.md
 cp $DEST/micro-stock-service/README.md docs/sources/micro-stock-service.md
 cp $DEST/micro-cart-service/README.md docs/sources/micro-cart-service.md
 cp $DEST/micro-tax-service/README.md docs/sources/micro-tax-service.md
 cp $DEST/micro-promotion-service/README.md docs/sources/micro-promotion-service.md
 cp $DEST/micro-promotion-service/Examples.md docs/sources/Examples.md
+cp $DEST/micro-personalization-service/README.md docs/sources/micro-personalization-service.md
 cp $DEST/micro-oauth-service/README.md docs/sources/micro-oauth-service.md
 
 cp -R $DEST/micro-catalog-service/src/docs/api docs/sources
+cp -R $DEST/micro-customer-service/src/docs/api docs/sources
 cp -R $DEST/micro-stock-service/src/docs/api docs/sources
 cp -R $DEST/micro-cart-service/src/docs/api docs/sources
 cp -R $DEST/micro-tax-service/src/docs/api docs/sources
 cp -R $DEST/micro-promotion-service/src/docs/api docs/sources
+cp -R $DEST/micro-personalization-service/src/docs/api docs/sources
 cp -R $DEST/micro-oauth-service/src/docs/api docs/sources
 
 npm run doc
