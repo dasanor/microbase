d00d8a4b84e7baae91bcb96dd44d2927ac402d1e
Nicolás Cornaglia <ncornag@gmail.com>
2016-12-28 21:44:16 +0100

    New config and util modules tests (incomplete)

--- src/test/test.js
+++ src/test/test.js
@@ -1,9 +1,7 @@
-const shortId = require('shortid');
-const moment = require('moment');
-
 const Code = require('code');
 const Lab = require('lab');
 const nock = require('nock');
+const sinon = require('sinon');
 
 // shortcuts
 const lab = exports.lab = Lab.script();
@@ -13,17 +11,87 @@ const after = lab.after;
 const it = lab.it;
 const expect = Code.expect;
 
-const base = require('../index.js');
+const fakeBase = {
+  log: (level, msg) => {
+    console.log(`${new Date().toISOString()} - ${level}: [] ${msg}`);
+  }
+};
 
-describe('Base', () => {
-  beforeEach(done => {
+const base = require('../index.js')({
+  config: require('../modules/config')([
+    './test/config.json'
+  ], fakeBase)
+});
+
+describe('Config', () => {
+  it('should handle a config file not found as a log', (done) => {
+    const spy = sinon.spy(fakeBase, 'log');
+    require('../modules/config')([
+      './test/fake.json'
+    ], fakeBase);
+    expect(spy.getCall(2).args[0]).equals('error');
+    expect(spy.getCall(2).args[1]).contains('[File doesn\'t exist]');
+    fakeBase.log.restore();
     done();
   });
-  after(done => {
+  it('should return a correct value', (done) => {
+    const value = base.config.get('services:defaultFolder');
+    expect(value).to.equal('operations');
     done();
   });
+  it('should return a correct env value', (done) => {
+    process.env['logger_level'] = 'warn'
+    const config = require('../modules/config')([], fakeBase);
+    const value = config.get('logger:level');
+    expect(value).to.equal('warn');
+    done();
+  });
+});
 
-  it('is a dummy test', done => {
+describe('Utils', () => {
+  it('should extract schema validation errors', (done) => {
+    const msg = 'should have required property \'title\'';
+    const error = {
+      name: 'ValidationError',
+      errors: [{
+        keyword: 'required',
+        dataPath: '',
+        schemaPath: '#/required',
+        params: { missingProperty: 'title' },
+        message: msg
+      }]
+    };
+
+    const errorList = base.utils.extractErrors(error);
+    expect(errorList).to.be.a.array().and.to.have.length(1);
+    expect(errorList[0]).to.be.equal(`payload: ${msg}`);
+    done();
+  });
+  it('should extract mongoose validation errors', (done) => {
+    const msg = 'Cast to Number failed for value \'A\' at path \'stockStatus\'';
+    const error = {
+      message: 'Product validation failed',
+      name: 'ValidationError',
+      errors: {
+        stockStatus: {
+          message: msg,
+          name: 'CastError',
+          kind: 'Number',
+          value: 'A',
+          path: 'stockStatus',
+          reason: {
+            message: msg,
+            name: 'CastError',
+            kind: 'number',
+            value: 'A',
+            path: 'stockStatus'
+          }
+        }
+      }
+    };
+    const errorList = base.utils.extractErrors(error);
+    expect(errorList).to.be.a.array().and.to.have.length(1);
+    expect(errorList[0]).to.be.equal(`stockStatus: ${msg}`);
     done();
   });
 });
