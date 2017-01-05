b0649c209e7aa3911dacb11713da0b6bcc4a70bf
Nicolás Cornaglia <ncornag@gmail.com>
2017-01-05 16:31:51 +0100

    Added cache and validator inMiddlerare tests

--- src/test/testInMiddlewares.js
+++ src/test/testInMiddlewares.js
@@ -33,16 +33,85 @@ const base = require('../index.js')({
   ], fakeBase)
 });
 
-describe('inMiddlewares', () => {
-  it('should xxx', (done) => {
+describe('inMiddleware', () => {
+  it('eventEmmiter should emit an event', (done) => {
     const params = { a: 1 };
     const channel = 'TEST';
-    const eem = require('../modules/services/inMiddlewares/eventEmitter')(base).handler({ channel: channel });
+    const handler = require('../modules/services/inMiddlewares/eventEmitter')(base)
+      .handler({ channel });
     base.bus.subscribe(`${channel}.*`, (msg) => {
       expect(msg).to.equal({ data: params });
       done();
     });
-    eem(params, undefined, undefined, () => {
+    handler(params, undefined, undefined, () => {
     });
+  });
+  it('validator should validate correct parameters', (done) => {
+    const params = { cartId: '001', firstName: 'John', lastName: 'Doe' };
+    const handler = require('../modules/services/inMiddlewares/validator')(base)
+      .handler({ schema: require('./schemas/testSchema') });
+    handler(params, undefined, undefined, () => {
+      done();
+    });
+  });
+  it('validator should validate required parameter', (done) => {
+    const params = { cartId: '001', firstName: 'John' };
+    const expectedError = {
+      ok: false,
+      error: 'validation_error',
+      data: ['payload: should have required property \'lastName\'']
+    };
+    const handler = require('../modules/services/inMiddlewares/validator')(base)
+      .handler({ schema: require('./schemas/testSchema') });
+    handler(params, (error) => {
+      expect(error).equals(expectedError);
+      done();
+    }, undefined, () => {
+      done('shouldn\'t be here');
+    });
+  });
+
+  it('cache should return cached response', (done) => {
+    const params = { id: 1 };
+    const expectedResponse = { hitTimes: 1 };
+    const cacheExpires = 1000;
+    const cacheOptions = {
+      options: {
+        expiresIn: cacheExpires
+      },
+      name: 'test',
+      keyGenerator: payload => payload.id
+    };
+    const handler = require('../modules/services/inMiddlewares/cache')(base)
+      .handler(cacheOptions);
+
+    let hitTimes = 0;
+    const originalFn = (params, reply) => {
+      hitTimes += 1;
+      reply({ hitTimes });
+    };
+
+    // Call the handler
+    handler(
+      params,
+      function reply(responsea) {
+        expect(responsea).equal(expectedResponse);
+        // Call again the handler after some time to allow the cache to store the value
+        setTimeout(() => {
+          handler(
+            params,
+            responseb => {
+              expect(responseb).equal(expectedResponse);
+              done();
+            },
+            { headers: [] },
+            newReply => originalFn(params, newReply)
+          );
+        }, 500);
+      },
+      { headers: [] },
+      newReply => originalFn(params, newReply)
+    );
+
   });
 });
